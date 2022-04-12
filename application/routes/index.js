bd748de2489d4dc80e5f5fc9165f6e63e92f4359
bearagones <baragones@mail.sfsu.edu>
2022-04-11 17:11:28 -0700

    Updated index.js

--- application/routes/index.js
+++ application/routes/index.js
@@ -3,11 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('home', { title: 'CSC 317 App', name:'Beatrice Aragones' });
+  res.render('index', { title: 'CSC 317 App', name:'Beatrice Aragones' });
 });
 
 router.get('/login', (req,res,next) => {
   res.render('login');
+})
+
+router.get('/registration', (req,res,next) => {
+  res.render('registration');
+})
+
+router.get('/postimage', (req,res,next) => {
+  res.render('postimage');
+})
+
+router.get('/viewpost', (req,res,next) => {
+  res.render('viewpost');
 })
 
 module.exports = router;
