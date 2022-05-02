078dd4e03c8672a60f3de2a31b51f519e7cbaa44
bearagones <baragones@mail.sfsu.edu>
2022-05-02 15:25:16 -0700

    Created posts.js

---
+++ application/routes/posts.js
@@ -0,0 +1,58 @@
+var express = require('express');
+var router = express.Router();
+var db = require("../config/database");
+const {successPrint , errorPrint} = require("../helpers/debug/debugprinters");
+var sharp = require('sharp');
+var multer = require('multer');
+var crypto = require('crypto');
+var PostError = require("../helpers/error/PostError");
+
+var storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, "public/images/uploads");
+    },
+    filename: function(req, file, cb) {
+        let fileExt = file.mimetype.split('/')[1];
+        let randomName = crypto.randomBytes(22).toString("hex");
+        cb(null, `${randomName}.${fileExt}`);
+    }
+});
+
+var uploader = multer({storage: storage});
+
+router.post('/createPost', uploader.single("selectImage"), (req, res, next) => {
+    let fileUploaded = req.file.path;
+    let fileAsThumbnail = `thumbnail-${req.file.fileName}`;
+    let destinationOfThumbnail = req.file.destination + "/" + fileAsThumbnail;
+    let title = req.body.title;
+    let description = req.body.description;
+    let fk_userId = req.session.userId;
+
+    sharp(fileUploaded)
+        .resize(200)
+        .toFile(destinationOfThumbnail)
+        .then(() => {
+            let baseSQL = 'INSERT INTO posts (title, description, photopath, thumbnail, created, fk_userId) VALUE (?,?,?,?,now(),?);'
+            return db.execute(baseSQL,[title, description, fileUploaded, destinationOfThumbnail, fk_userId]);
+        })
+        .then(([results, fields]) => {
+            if (results && results.affectedRows) {
+                successPrint("New post was created!");
+                res.json({status: "OK", message: "Your post was created successfully!", "redirect": "/"});
+            } else {
+                res.json({status: "OK", message: "Your post was not created!", "redirect": "/postimage"});
+            }
+        })
+        .catch((err) => {
+            if (err instanceof PostError) {
+                errorPrint(err.getMessage());
+                req.flash('error', err.getMessage());
+                res.status(err.getStatus());
+                res.redirect(err.getRedirectURL());
+            } else {
+                next(err);
+            }
+        })
+});
+
+module.exports = router;
