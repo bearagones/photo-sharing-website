59629d32444f7fc233439994d6012dfeb173f12c
bearagones <baragones@mail.sfsu.edu>
2022-05-03 00:08:00 -0700

    Updated index.js

--- application/routes/index.js
+++ application/routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var isLoggedIn = require('../middleware/routeprotectors.js').userIsLoggedIn;
 var getRecentPosts = require('../middleware/postsmiddleware.js').getRecentPosts;
+var db = require('../config/database.js');
 
 /* GET home page. */
 router.get('/', getRecentPosts, function(req, res, next) {
@@ -22,7 +23,19 @@ router.get('/postimage', (req,res,next) => {
 })
 
 router.get('/post/:id(\\d+)', (req,res,next) => {
-  res.send({params:req.params.id});
+  let baseSQL = "SELECT u.username, p.title, p.description, p.photopath, p.created FROM users u JOIN posts p ON u.id=fk_userId WHERE p.id=?;";
+  let postId = req.params.id;
+
+  db.execute(baseSQL, [postId])
+  .then(([results, fields]) => {
+    if (results && results.length) {
+      let post = results[0];
+      res.render('viewpost', {currentPost: post});
+    } else {
+      req.flash('error', "This is not the post you are looking for!");
+      res.redirect('/');
+    }
+  })
 })
 
 module.exports = router;
