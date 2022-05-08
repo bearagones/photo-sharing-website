c1de15955ce4f2d1fa1ede31a4ecd12eb936ba91
bearagones <baragones@mail.sfsu.edu>
2022-05-07 22:19:20 -0700

    Updated index.js

--- application/routes/index.js
+++ application/routes/index.js
@@ -1,12 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var isLoggedIn = require('../middleware/routeprotectors.js').userIsLoggedIn;
-var getRecentPosts = require('../middleware/postsmiddleware.js').getRecentPosts;
-var db = require('../config/database.js');
+const {getRecentPosts, getPostById, getCommentsByPostId} = require('../middleware/postsmiddleware.js');
 
 /* GET home page. */
 router.get('/', getRecentPosts, function(req, res, next) {
-  res.render('index', { title: 'CSC 317 App'});
+  res.render('index', { title: 'AmiguruBea'});
 });
 
 router.get('/login', (req,res,next) => {
@@ -22,20 +21,8 @@ router.get('/postimage', (req,res,next) => {
   res.render('postimage', {title: "Post an Image"});
 })
 
-router.get('/post/:id(\\d+)', (req,res,next) => {
-  let baseSQL = "SELECT u.username, p.title, p.description, p.photopath, p.created FROM users u JOIN posts p ON u.id=fk_userId WHERE p.id=?;";
-  let postId = req.params.id;
-
-  db.execute(baseSQL, [postId])
-  .then(([results, fields]) => {
-    if (results && results.length) {
-      let post = results[0];
-      res.render('viewpost', {currentPost: post});
-    } else {
-      req.flash('error', "This is not the post you are looking for!");
-      res.redirect('/');
-    }
-  })
+router.get('/post/:id(\\d+)', getPostById, getCommentsByPostId, (req,res,next) => {
+    res.render('viewpost', {title: `Post ${req.params.id}`});
 })
 
 module.exports = router;
