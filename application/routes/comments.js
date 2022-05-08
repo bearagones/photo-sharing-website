d2fecbfa47cb16c9b5f7af8a98f32aa27673da79
bearagones <baragones@mail.sfsu.edu>
2022-05-07 22:19:57 -0700

    Created comments.js

---
+++ application/routes/comments.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+const {successPrint, errorPrint} = require('../helpers/debug/debugprinters');
+const {create} = require('../models/commentsmodel');
+
+router.post('/create', (req, res, next) => {
+    if (!req.session.username) {
+        errorPrint("Must be logged in to comment!");
+        req.json({
+            code: -1, status: "danger", message: "Must be logged in to create a comment!"
+        })
+    } else {
+        let {comment, postId} = req.body;
+        let username = req.session.username;
+        let userId = req.session.userId;
+
+        create(userId, postId, comment)
+        .then((wasSuccessful) => {
+            if (wasSuccessful !== -1) {
+                successPrint(`Comment was created for ${username}`);
+                res.json({
+                    code: 1, status: "success", message: "Comment created!", comment: comment, username: username
+                })
+            } else {
+                errorPrint("Comment was not saved!");
+                res.json({
+                    code: -1, status: "danger", message: "Message was not created!"
+                })
+            }
+        })
+        .catch((err) => next(err));
+    }
+})
+
+module.exports = router;
