622886a6471648bcaac643410f6038eb67ccb7f9
bearagones <baragones@mail.sfsu.edu>
2022-05-12 13:21:16 -0700

    Updated validation.js
    - implemented login/registration/post validation

--- application/middleware/validation.js
+++ application/middleware/validation.js
@@ -1,24 +1,68 @@
-const checkUsername  = (username) => {
+const inputValidation = (input) => {
+    if (input) {
+        return true;
+    }
+}
+
+const usernameValidation = (username) => {
     let usernameSpec = /^[a-zA-Z][a-zA-Z0-9]{2,}$/;
+
     return usernameSpec.test(username);
 }
 
-const checkPassword = (password) => {
+const emailValidation = (email) => {
+    let emailSpec = /\S+@\S+\.\S+/;
+
+    return emailSpec.test(email);
+}
+
+const passwordValidation = (password) => {
     let passwordSpec = /^(?=.*[A-Z])(?=.*\d)(?=.*[-/*+!@#$^&]).{7,}$/;
+
     return passwordSpec.test(password);
+}
+
+const imageValidation = (image) => {
+    let imageSpec = /(\.jpg|\.jpeg|\.png|\.bmp|\.gif)$/i;
+
+    return imageSpec.test(image);
 }
 
 const registerValidation = (req, res, next) => {
     let username = req.body.username;
+    let email = req.body.email;
     let password = req.body.password;
+    let confirmPassword = req.body.confirmpass;
+    let attestAge = req.body.age;
+    let readAndAgree = req.body.tos;
 
-    if (!checkUsername(username)) {
-        req.flash('error', "Invalid username!");
+    if (!inputValidation(username) || !inputValidation(email) || !inputValidation(password) || !inputValidation(confirmPassword)) {
+        req.flash('error', "Must fill in empty field(s)!");
         req.session.save(err => {
             res.redirect('/registration');
         })
-    } else if (!checkPassword(password)) {
-        req.flash('error', "Invalid password!");
+    } else if (!usernameValidation(username)) {
+        req.flash('error', "Username must be 3 or more alphanumeric characters!");
+        req.session.save(err => {
+            res.redirect('/registration');
+        })
+    } else if (!emailValidation(email)) {
+        req.flash('err', "Email must be in the form of 'something@something.something'!");
+        req.session.save(err => {
+            res.redirect('/registration');
+        })
+    } else if (!passwordValidation(password)) {
+        req.flash('error', "Password must be 8 or more characters with a number, uppercase letter, and special character!");
+        req.session.save(err => {
+            res.redirect('/registration');
+        })
+    } else if (password !== confirmPassword) {
+        req.flash('error', "Passwords do not match!");
+        req.session.save(err => {
+            res.redirect('/registration');
+        })
+    } else if (!(attestAge || readAndAgree)) {
+        req.flash('error', "Must check the boxes to proceed!");
         req.session.save(err => {
             res.redirect('/registration');
         })
@@ -28,7 +72,42 @@ const registerValidation = (req, res, next) => {
 }
 
 const loginValidation = (req, res, next) => {
+    let username = req.body.username;
+    let password = req.body.password;
 
+    if (!inputValidation(username) || !inputValidation(password)) {
+        req.flash('error', "Must fill in empty field(s)!");
+        req.session.save(err => {
+            res.redirect('/login');
+        })
+    } else if (!usernameValidation(username)) {
+        req.flash('error', "Invalid username!");
+        req.session.save(err => {
+            res.redirect('/login');
+        })
+    } else if (!passwordValidation(password)) {
+        req.flash('error', "Invalid password!");
+        req.session.save(err => {
+            res.redirect('/login');
+        })
+    } else {
+        next();
+    }
 }
 
-module.exports = {registerValidation, loginValidation};
+const postValidation = (req, res, next) => {
+    let title = req.body.title;
+    let description = req.body.description;
+    let policy = req.body.policy;
+
+    if (!inputValidation(title) || !inputValidation(description) || !inputValidation(policy)) {
+        req.flash('error', "Must fill in empty field(s)!");
+        req.session.save(err => {
+            res.redirect('/postimage');
+        })
+    } else {
+        next();
+    }
+}
+
+module.exports = {registerValidation, loginValidation, postValidation};
