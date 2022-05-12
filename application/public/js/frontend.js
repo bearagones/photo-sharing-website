ad1ff1b3bd3dabc17bac9ce1536d9a9c495d7fb5
bearagones <baragones@mail.sfsu.edu>
2022-05-12 13:23:07 -0700

    Updated frontend.js
    - implemented functions for comments and searches

--- application/public/js/frontend.js
+++ application/public/js/frontend.js
@@ -37,6 +37,7 @@ function createCard(postData) {
 
 function executeSearch() {
     let searchTerm = document.getElementById("search-text").value;
+    document.getElementById("search-text").value = "";
     if (!searchTerm) {
         location.replace('/');
         return;
@@ -44,20 +45,59 @@ function executeSearch() {
     let gridContainer = document.getElementById("grid-container");
     let searchURL = `/posts/search?search=${searchTerm}`;
     fetch(searchURL)
-    .then((data) => {
-        return data.json();
-    })
-    .then((data_json) => {
-        let newGridContainerHTML = '';
-        data_json.results.forEach((row) => {
-            newGridContainerHTML += createCard(row);
+        .then((data) => {
+            return data.json();
         })
-        gridContainer.innerHTML = newGridContainerHTML;
-        if(data_json.message) {
-            addFlashFromFrontEnd(data_json.message);
-        }
-    })
-    .catch((err) => console.log(err));
+        .then((data_json) => {
+            let newGridContainerHTML = '';
+            data_json.results.forEach((row) => {
+                newGridContainerHTML += createCard(row);
+            })
+            gridContainer.innerHTML = newGridContainerHTML;
+            if (data_json.message) {
+                addFlashFromFrontEnd(data_json.message);
+            }
+        })
+        .catch((err) => console.log(err));
+}
+
+function postComment() {
+    let commentText = document.getElementById("comment-text").value;
+    let postId = document.location.pathname.match(/\d+/g).map(Number)[0];
+    if (!commentText) {
+        return;
+    }
+    let fetchOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "Application/json"
+        },
+        body: JSON.stringify({
+            comment: commentText, postId: postId
+        })
+    };
+    fetch('/comments/create', fetchOptions)
+        .then((response) => response.json())
+        .then((data) => {
+            if (data.code === 1) {
+                addMessage(data);
+            } else {
+                addFlashFromFrontEnd(data.message, data.status);
+            }
+        })
+        .catch((err) => console.log(err));
+}
+
+function addMessage(data) {
+    let template = document.createElement("template");
+    template.innerHTML =
+        `<div id="message-${data.id}}">
+            <strong class="author-text">@${data.username}</strong>
+            <span class="date-posted">${new Date().toLocaleString()}</span>
+            <div class="comment">${data.comment}</div>
+        </div>`
+    document.getElementById("comment-section").firstChild.before(template.content.firstChild);
+    document.getElementById("comment-text").value = "";
 }
 
 let flashMessageElement = document.getElementById("flash-message");
@@ -71,9 +111,22 @@ if (searchButton) {
 }
 
 let searchText = document.getElementById("search-text");
-searchText.addEventListener("keyup", function(event) {
+searchText.addEventListener("keyup", function (event) {
     event.preventDefault();
     if (event.keyCode === 13) {
         searchButton.click();
     }
 });
+
+let commentButton = document.getElementById("comment-button");
+if (commentButton) {
+    commentButton.onclick = postComment;
+}
+
+let commentText = document.getElementById("comment-text");
+commentText.addEventListener("keyup", function (event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+        commentButton.click();
+    }
+})
